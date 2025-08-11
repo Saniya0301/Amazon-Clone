7bd1a2beab650d1291d7688dccda68d1245b49d8
Saniya0301 <saniyachhabra3@gmail.com>
2025-08-11 13:57:58 +0530

    Add files via upload

---
+++ src/Component/Navbar/navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+src/Component/Navbar/NavbarBanner/navbarBanner.js
+
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      {/* please watch the video for code */}
+        <NavbarBelt/>
+        <NavbarBanner/>
+    </div>
+  )
+}
+
+export default Navbar
